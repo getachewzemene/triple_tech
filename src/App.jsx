c1ed29da885c875348ed2d7aee48b2dd07638e2d
getachew <getchze1221@gmail.com>
2025-07-10 17:45:13 +0300

    fix: Change Router import from BrowserRouter to HashRouter for better routing support

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 import {ThemeContext } from "./ThemeContext";
 import Footer from "./components/Footer";
